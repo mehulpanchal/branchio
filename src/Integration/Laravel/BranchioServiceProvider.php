0ebbc70ede4477c57ed45ee91c612c6340eeab8a
mehulpanchal <mehul.panchal@mydiabeteshome.com>
2021-07-23 15:42:33 +0530

    guzzle http version changed

--- src/Integration/Laravel/BranchioServiceProvider.php
+++ src/Integration/Laravel/BranchioServiceProvider.php
@@ -8,30 +8,18 @@ class BranchioServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        // $this->app->bind(\Iivannov\Branchio\Client::class, function ($app) {
+        $this->app->bind(\Iivannov\Branchio\Client::class, function ($app) {
 
-        //     /** @var @see \Illuminate\Config\Repository $config */
-        //     $config = $app->make('config');
+            /** @var @see \Illuminate\Config\Repository $config */
+            $config = $app->make('config');
 
-        //     $key = $config->get('services.branchio.key');
-        //     $secret = $config->get('services.branchio.secret');
+            $key = $config->get('services.branchio.key');
+            $secret = $config->get('services.branchio.secret');
 
-        //     if (!$key || !$secret)
-        //         throw new \InvalidArgumentException('Missing BranchIO key and secret in configuration files');
+            if (!$key || !$secret)
+                throw new \InvalidArgumentException('Missing BranchIO key and secret in configuration files');
 
-        //     return new \Iivannov\Branchio\Client($key, $secret);
-        // });
-        $app = app();
-        $config = $app->make('config');
-        $key = $config->get('services.branchio.key');
-        $secret = $config->get('services.branchio.secret');
-        if (!$key || !$secret) {
-            throw new \InvalidArgumentException('Missing BranchIO key and secret in configuration files');
-            new \Iivannov\Branchio\Client($key, $secret);
-        }
-        $branch = new \Iivannov\Branchio\Client($key, $secret);
-        $this->app->bindIf(\Iivannov\Branchio\Client::class, function () use ($branch) {
-            return $branch;
+            return new \Iivannov\Branchio\Client($key, $secret);
         });
     }
 }
